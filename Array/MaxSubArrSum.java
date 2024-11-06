76d1e33ca8228042094933d0e5fe72690c9c1090
shristi8299 <shristi7880@gmail.com>
2024-11-06 09:09:37 +0530

    maxsum of subarray brouteforce approach

---
+++ Array/MaxSubArrSum.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class MaxSubArrSum{
+    public static void maxSubArrSum(int arr[]){
+        int currSum;
+        int maxSum = Integer.MIN_VALUE;
+        for(int i = 0; i < arr.length; i++){
+            int start = i;
+            for(int j = i; j < arr.length; j++){
+                int end = j;
+                currSum = 0;
+                for(int k = start; k <= end; k++){
+                    currSum += arr[k];
+                }
+                System.out.println(currSum);
+                if(maxSum < currSum){
+                    maxSum = currSum;
+                }
+            }
+            System.out.println();
+        }
+        System.out.println("maxsum of subarr " + maxSum);
+    }
+
+    public static void main(String args[]){
+        int arr[] = {2, 4, 6, 8, 10};
+        maxSubArrSum(arr);
+    }
+}
